b952864234af556bf7c0bc18397474c293636de9
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-30 23:28:51 +0530

    init commit

---
+++ src/test/java/com/spring/webfluxrest/WebfluxRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.webfluxrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
