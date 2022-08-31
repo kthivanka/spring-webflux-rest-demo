3915a5fe1107450df1a13a7646bb26fdd2561340
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-31 17:03:09 +0530

    add CategoryController Constructor

--- src/main/java/com/spring/webfluxrest/controllers/CategoryController.java
+++ src/main/java/com/spring/webfluxrest/controllers/CategoryController.java
@@ -1,7 +1,13 @@
 package com.spring.webfluxrest.controllers;
 
+import com.spring.webfluxrest.repositories.CategoryRepository;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CategoryController {
+    private CategoryRepository vendorRepository;
+
+    public CategoryController(CategoryRepository vendorRepository) {
+        this.vendorRepository = vendorRepository;
+    }
 }
