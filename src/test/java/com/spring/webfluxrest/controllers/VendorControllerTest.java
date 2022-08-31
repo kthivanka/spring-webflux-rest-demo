81505d8844d85ba526a084476373ebe4ba4f871b
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-31 18:39:15 +0530

    impl tests for Vendor Controller

---
+++ src/test/java/com/spring/webfluxrest/controllers/VendorControllerTest.java
@@ -0,0 +1,47 @@
+package com.spring.webfluxrest.controllers;
+
+import com.spring.webfluxrest.domain.Vendor;
+import com.spring.webfluxrest.repositories.VendorRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+
+class VendorControllerTest {
+
+    WebTestClient webTestClient;
+    VendorRepository vendorRepository;
+    VendorController vendorController;
+
+    @BeforeEach
+    void setUp() {
+        vendorRepository = Mockito.mock(VendorRepository.class);
+        vendorController = new VendorController(vendorRepository);
+        webTestClient = WebTestClient.bindToController(vendorController).build();
+    }
+
+    @Test
+    void getAllVendors() {
+        BDDMockito.given(vendorRepository.findAll()).willReturn(Flux
+                .just(Vendor.builder()
+                                .firstName("Davy")
+                                .lastName("Jones")
+                                .build(),
+                        Vendor.builder()
+                                .firstName("Jack")
+                                .lastName("Sparrow")
+                                .build()
+                ));
+        webTestClient.get()
+                .uri("/api/v1/vendors")
+                .exchange()
+                .expectBodyList(Vendor.class)
+                .hasSize(2);
+    }
+
+    @Test
+    void getVendorById() {
+    }
+}
