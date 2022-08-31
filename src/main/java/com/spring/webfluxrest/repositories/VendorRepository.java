8639729873ba8bf473e7d2f8d2d568d6b676e1cf
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-31 15:50:39 +0530

    add vendor repository

---
+++ src/main/java/com/spring/webfluxrest/repositories/VendorRepository.java
@@ -0,0 +1,7 @@
+package com.spring.webfluxrest.repositories;
+
+import com.spring.webfluxrest.domain.Vendor;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface VendorRepository extends ReactiveMongoRepository<Vendor, String> {
+}
