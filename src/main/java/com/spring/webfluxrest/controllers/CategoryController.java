eed9cbdd78bff19d5d111f4f86709b85193bbe5f
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-31 17:40:33 +0530

    add getAllCategories

--- src/main/java/com/spring/webfluxrest/controllers/CategoryController.java
+++ src/main/java/com/spring/webfluxrest/controllers/CategoryController.java
@@ -3,8 +3,10 @@ package com.spring.webfluxrest.controllers;
 import com.spring.webfluxrest.domain.Category;
 import com.spring.webfluxrest.repositories.CategoryRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class CategoryController {
@@ -14,5 +16,9 @@ public class CategoryController {
         this.categoryRepository = categoryRepository;
     }
 
+    @GetMapping("/api/v1/categories")
+    Flux<Category> getAllCategories(){
+        return categoryRepository.findAll();
+    }
 
 }
