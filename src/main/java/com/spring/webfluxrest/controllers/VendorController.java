1a4030950850714ef4e8294f61fb2f5b4854998e
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-31 18:41:41 +0530

    impl getAllVendors(), getVendorById()

--- src/main/java/com/spring/webfluxrest/controllers/VendorController.java
+++ src/main/java/com/spring/webfluxrest/controllers/VendorController.java
@@ -1,7 +1,12 @@
 package com.spring.webfluxrest.controllers;
 
+import com.spring.webfluxrest.domain.Vendor;
 import com.spring.webfluxrest.repositories.VendorRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class VendorController {
@@ -9,5 +14,15 @@ public class VendorController {
 
     public VendorController(VendorRepository vendorRepository) {
         this.vendorRepository = vendorRepository;
+    }
+
+    @GetMapping("/api/v1/vendors")
+    Flux<Vendor> getAllVendors(){
+        return vendorRepository.findAll();
+    }
+
+    @GetMapping("/api/v1/vendors/{id}")
+    Mono<Vendor> getVendorById(@PathVariable String id){
+        return vendorRepository.findById(id);
     }
 }
