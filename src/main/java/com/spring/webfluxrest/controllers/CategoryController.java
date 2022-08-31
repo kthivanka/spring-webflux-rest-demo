52814a6897ce379cbef3c961d739cd1603f53880
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-31 17:06:41 +0530

    change CategoryController Constructor name

--- src/main/java/com/spring/webfluxrest/controllers/CategoryController.java
+++ src/main/java/com/spring/webfluxrest/controllers/CategoryController.java
@@ -1,13 +1,18 @@
 package com.spring.webfluxrest.controllers;
 
+import com.spring.webfluxrest.domain.Category;
 import com.spring.webfluxrest.repositories.CategoryRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class CategoryController {
-    private CategoryRepository vendorRepository;
+    private CategoryRepository categoryRepository;
 
-    public CategoryController(CategoryRepository vendorRepository) {
-        this.vendorRepository = vendorRepository;
+    public CategoryController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
     }
+
+
 }
