9b0c03986eaa0dea24849e61371b6cb83ca51d12
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-31 17:56:17 +0530

    add test for CategoryController

---
+++ src/test/java/com/spring/webfluxrest/controllers/CategoryControllerTest.java
@@ -0,0 +1,21 @@
+package com.spring.webfluxrest.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllCategories() {
+    }
+
+    @Test
+    void getCategoryById() {
+    }
+}
